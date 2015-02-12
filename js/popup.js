f05b65fb6c6fd24c5522d2f2c04122ba86feb4c2
QLing <liminadd@gmail.com>
2015-02-12 18:01:32 +0800

    a

--- js/popup.js
+++ js/popup.js
@@ -1,24 +1,24 @@
 try{
-                var xmlhttp = new window.XMLHttpRequest();
-        xmlhttp.open("GET","http://animetime.qiniudn.com/emotion.xml",false);
-        xmlhttp.send(null);
-        //alert(xmlhttp.readyState);
-		//alert("弹窗测试")
-        if(xmlhttp.readyState == 4){
-        xmlDoc = xmlhttp.responseXML.documentElement;
+        var xmlhttp = new window.XMLHttpRequest();
+          xmlhttp.open("GET","http://animetime.qiniudn.com/emotion.xml",false);
+          xmlhttp.send(null);
+          //alert(xmlhttp.readyState);    //返回读取状态
+		if(xmlhttp.readyState == 4){
+          //xmlDoc = xmlhttp.responseXML.documentElement;
+		  alert("浏览器支持该解析方式!");
+          alert(xmlhttp.responsetext);
+		  domParser = new  DOMParser();
+          xmlDoc = domParser.parseFromString(xmlString, 'text/xml');
         }
-        
             }catch(e){
-            alert("谷歌浏览器不支持该解析方式!");
-            }                  //可以工作
-//xmlDoc= document.implementation.createDocument("","",null);
-//xmlDoc.async= "false";
+            alert("浏览器不支持该解析方式!");
+            }     		
+//xmlDoc= document.implementation.createDocument("","",null);    //销毁，防止内存泄漏
 //xmlDoc= xmlDoc.loadXML("http://animetime.qiniudn.com/emotion.xml");
 //xmlDoc=loadXMLDoc("http://animetime.qiniudn.com/emotion.xml");
 //xmlDoc=loadXMLDoc("/source/KT.xml");
 var x= xmlDoc.documentElement.childNodes;
-alert(xmlDoc);
-alert(x.childNodes);         //chilaNodes对Chrome
+alert(x.text);         //chilaNodes对Chrome
   for (i=1;i<x.length;i++){
 	var y= x[i];
       for (n=0;n<y.length;n++){
