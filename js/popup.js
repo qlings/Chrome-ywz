90e003ef6f51e37149c6b5102abe92aefab3cfa3
QLing <liminadd@gmail.com>
2015-02-13 12:02:57 +0800

    a

--- js/popup.js
+++ js/popup.js
@@ -1,29 +1,15 @@
-try{
-        var xmlhttp = new window.XMLHttpRequest();
-          xmlhttp.open("GET","http://animetime.qiniudn.com/emotion.xml",false);
-          xmlhttp.send(null);
-          //alert(xmlhttp.readyState);    //返回读取状态
-		if(xmlhttp.readyState == 4){
-          //xmlDoc = xmlhttp.responseXML.documentElement;
-		  alert("浏览器支持该解析方式!");
-          alert(xmlhttp.responsetext);
-		  domParser = new  DOMParser();
-          xmlDoc = domParser.parseFromString(xmlString, 'text/xml');
-        }
-            }catch(e){
-            alert("浏览器不支持该解析方式!");
-            }     		
-//xmlDoc= document.implementation.createDocument("","",null);    //销毁，防止内存泄漏
-//xmlDoc= xmlDoc.loadXML("http://animetime.qiniudn.com/emotion.xml");
-//xmlDoc=loadXMLDoc("http://animetime.qiniudn.com/emotion.xml");
-//xmlDoc=loadXMLDoc("/source/KT.xml");
-var x= xmlDoc.documentElement.childNodes;
-alert(x.text);         //chilaNodes对Chrome
-  for (i=1;i<x.length;i++){
-	var y= x[i];
-      for (n=0;n<y.length;n++){
-        var z= y[n].childNodes;
-	      document.getElementById("facemoodbox").innerHTML= z.childNodes[0].text+","+z.chilaNodes[1].text;
-	      alert(z.text);
-	 }
-  }	  
+xmlhttp=new XMLHttpRequest();
+xmlhttp.open("GET","http://animetime.qiniudn.com/emotion.xml",false);
+xmlhttp.send();
+xmlDoc=xmlhttp.responseXML;
+//alert(xmlhttp.readyState);
+//alert(xmlDoc.responseXML);    //一直报错
+var x=xmlDoc.getElementsByTagName("category")[0];
+alert(x.tagName);               //通过x的name检查XML是否加载成功
+var txt = x.getElementsByTagName("entry")[0].childNodes[0].textContent;
+alert(x.getElementsByTagName("entry")[0].length);
+function display()
+{
+txta= txt+"baiqoain";
+document.getElementById("suggest").innerText=txta;
+}
