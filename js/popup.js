e561f25c3869259ff9d8366a980a08f0ac8f3d75
QLing <liminadd@gmail.com>
2015-02-14 19:02:35 +0800

    a

--- js/popup.js
+++ js/popup.js
@@ -1,19 +1,12 @@
+var url="http://animetime.qiniudn.com/emotion.xml";
 xmlhttp=new XMLHttpRequest();
-xmlhttp.open("GET","http://animetime.qiniudn.com/emotion.xml",false);
+xmlhttp.open("GET",url,false);
 xmlhttp.send();
 xmlDoc=xmlhttp.responseXML;
-//xmlDoc=loadXMLString(text);
-//alert(xmlhttp.readyState);
-alert(xmlDoc.responseXML); 
-//alert(xmlDoc.responseXML);    //一直报错
 var x=xmlDoc.getElementsByTagName("category")[0];
-alert(x.tagName);               //通过x的name检查XML是否加载成功
-var txt = x.getElementsByTagName("entry")[0].childNodes[0].textContent;
-y=x.getElementsByTagName("entry")[0].childNodes[1];
-alert(x.getElementsByTagName("entry").length);   //category的子节点：entry节点数量22
-alert(y.textContent);                       //Chrome已经不支持nodeValue，使用textContent替代以显示文本
-/* function display()
+var txt = x.getElementsByTagName("entry")[1].childNodes[3];
+alert(txt.textContent);
+function display()
 {
-txta= txt+"baiqoain";
-document.getElementById("suggest").innerText=txta;
-} */
+document.getElementById("tagbox").innerText=txt.textContent;   
+}
