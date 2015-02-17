b66c72fd3f00ee2fe44913af316d508a53e46574
QLing <liminadd@gmail.com>
2015-02-17 21:56:40 +0800

    a

--- js/options.js
+++ js/options.js
@@ -1,1 +1,13 @@
 //设置页的js
+function saveset(){
+	var SourceUrl;
+	var setsource = document.getElementsByName("source");
+  for(i=0;i<setsource.length;i++){
+	  if(setsource[i].checked){
+		  SourceUrl = setsource[i].value;
+		  alert(SourceUrl);
+		  dress.href.innerText = SourceUrl;
+		  save4.innerText = SourceUrl;
+	    }
+    }
+}
